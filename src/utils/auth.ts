ef90afa6ccea0550ad88330192a9cea0cef7b991
Etornam <navidben45@gmail.com>
2025-08-11 17:33:28 +0000

    use the Reponse constructor

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,32 +3,31 @@ import { JWT } from "./jwt";
 import { eq } from "drizzle-orm";
 import { db } from "./db";
 import { users } from "../db/user";
+import { UNAUTHORIZED } from "./response";
 
 
 export const AuthMiddleware = new Elysia()
-    .use(JWT)
-    .macro({
-        userAuth: {
-            resolve: async ({ jwt, cookie: { auth }, set }) => {
-                try {
-                    const data = await jwt.verify(auth.value) as { id: string }
-                    if (!data) {
-                        throw new Error("Undefined")
-                    }
-                    const _user = (await db.select().from(users).where(eq(users.id, data.id)))[0]
-                    if (!_user) {
-                        throw new Error("Undefined")
-                    }
-                    console.log("USER   ",_user)
-                    return {
-                        user: _user
-                    }
-                } catch (error) {
-                    set.status = 401
-                    return {
-                        message: "Unauthorized"
-                    }
-                }
-            }
+  .use(JWT)
+  .macro({
+    userAuth: {
+      resolve: async ({ jwt, cookie: { auth }, set }) => {
+        try {
+          const data = await jwt.verify(auth.value) as { id: string }
+          if (!data) {
+            throw new Error("Undefined")
+          }
+          const _user = (await db.select().from(users).where(eq(users.id, data.id)))[0]
+          if (!_user) {
+            throw new Error("Undefined")
+          }
+          console.log("USER   ", _user)
+          return {
+            user: _user
+          }
+        } catch (error) {
+          set.status = 401
+          return UNAUTHORIZED("Unauthorized access, please login again.")
         }
-    })
+      }
+    }
+  })
