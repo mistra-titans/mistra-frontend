5a0990305cfee1e5e28b39a549d39c9fba59978b
Etornam <navidben45@gmail.com>
2025-08-11 17:32:17 +0000

    Add fname, lname with random UUID users table

--- src/db/user.ts
+++ src/db/user.ts
@@ -1,8 +1,9 @@
 import { pgTable, text, uuid } from "drizzle-orm/pg-core";
 
 export const users = pgTable('users', {
-  id: uuid('id').primaryKey(),
-  name: text('name').notNull(),
+  id: uuid('id').primaryKey().defaultRandom(),
+  first_name: text('first_name').notNull(),
+  last_name: text('last_name').notNull(),
   email: text('email').notNull().unique(),
   phone: text('phone').notNull(),
   created_at: text('created_at').notNull(),
