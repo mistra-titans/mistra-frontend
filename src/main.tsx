101df6826ae153308bde6ab5911ec65fa0d8a454
UnknownStrange <princenyarkoedwin@gmail.com>
2025-08-12 00:25:34 +0000

    authentication screen

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,20 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./pages/contexts/authcontext.tsx";
+import SignUp from "./pages/auth/signup.tsx";
+import ForgotPassword from "./pages/auth/forget.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/signup" element={<SignUp/>}/>
+          <Route path="/forget" element={<ForgotPassword/>}/>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>
 );
