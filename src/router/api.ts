93a94db90668d549a25dcb514b234323bbc3b428
Etornam <navidben45@gmail.com>
2025-08-12 19:29:25 +0000

    add both transaction and account routes

--- src/router/api.ts
+++ src/router/api.ts
@@ -1,8 +1,12 @@
 import Elysia from "elysia";
 import { AUTH_ROUTER } from "./auth";
+import { TRANSACTION_ROUTER } from "./transaction";
+import { ACCOUNT_ROUTER } from "./account";
 
 
 export const API_ROUTER = new Elysia({
   prefix: "/api",
 })
 .use(AUTH_ROUTER)
+.use(TRANSACTION_ROUTER)
+.use(ACCOUNT_ROUTER)
