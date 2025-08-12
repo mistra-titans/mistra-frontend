7b2530e9915211310cb190b6c44f3efc1a0d1e58
PapaYawAsiamah <99272425+PapaYawAsiamah@users.noreply.github.com>
2025-08-12 00:54:07 +0000

    fix: correct import path for SignUp component and add Activity route

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,9 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./pages/contexts/authcontext.tsx";
-import SignUp from "./pages/auth/signup.tsx";
+import SignUp from "./pages/auth/SignUp.tsx";
 import ForgotPassword from "./pages/auth/forget.tsx";
+import Activity from "./pages/activity.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -15,6 +16,7 @@ createRoot(document.getElementById("root")!).render(
           <Route path="/" element={<App />} />
           <Route path="/signup" element={<SignUp/>}/>
           <Route path="/forget" element={<ForgotPassword/>}/>
+          <Route path="/activity" element={<Activity/>}/>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
