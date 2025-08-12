e22d2b94b317e88a4e94b4c258a7bbb2b47d8328
PapaYawAsiamah <99272425+PapaYawAsiamah@users.noreply.github.com>
2025-08-12 02:10:12 +0000

    feat: update Sidebar navigation and add new pages for Home, Profile, Payment, and Transactions

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,10 @@ import { AuthProvider } from "./pages/contexts/authcontext.tsx";
 import SignUp from "./pages/auth/SignUp.tsx";
 import ForgotPassword from "./pages/auth/forget.tsx";
 import Activity from "./pages/activity.tsx";
+import Profile from "./pages/profile.tsx";
+import Payment from "./pages/payment.tsx";
+import Home from "./pages/home.tsx";
+import Transactions from "./pages/transactions.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -17,6 +21,10 @@ createRoot(document.getElementById("root")!).render(
           <Route path="/signup" element={<SignUp/>}/>
           <Route path="/forget" element={<ForgotPassword/>}/>
           <Route path="/activity" element={<Activity/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/payment" element={<Payment/>}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/transactions" element={<Transactions/>}/>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
